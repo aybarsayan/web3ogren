20f79e60b35ceac1a5d5f2d22101161225b21df2
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-12-16 20:41:52 +0300

    Category section is done

--- src/components/HomepageFeatures/category/Category.jsx
+++ src/components/HomepageFeatures/category/Category.jsx
@@ -1,6 +1,72 @@
-import "./Category.css";
-import React from "react";
+import "./css/Category.css";
+import React, { useEffect, useState } from "react";
+import SubCategory from "./SubCategory";
 
-export default function Category() {
- return <div></div>;
+export default function Category(category) {
+ const [showAll, setShowAll] = useState(false);
+ const [size, setSize] = useState(4);
+
+ useEffect(() => {
+  if (showAll)
+   incrementToTarget(
+    4,
+    category.category.subCategories.length,
+    showAll,
+    (value) => {
+     setSize(value);
+    }
+   );
+  else setSize(4);
+ }, [showAll]);
+
+ return (
+  <div
+   style={{
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "end",
+    gap: "15px",
+   }}
+  >
+   <label
+    style={{
+     paddingRight: "10px",
+     opacity: "80",
+     fontSize: "14px",
+     cursor: "pointer",
+    }}
+    onClick={() => setShowAll(!showAll)}
+   >
+    {showAll ? "Gizle" : "Hepsini Göster"}
+   </label>
+   <div className="cateogry-container">
+    <div className="category-h1-container">
+     <h1 style={{ textAlign: "center", fontSize: "28px" }}>
+      {category.category.title}
+     </h1>
+    </div>
+    <div className="category-sub-category-container">
+     {category.category?.subCategories
+      .slice(0, size)
+      .map((SubCategoryData, index) => {
+       return <SubCategory key={index} SubCategoryData={SubCategoryData} />;
+      })}
+    </div>
+   </div>
+  </div>
+ );
+}
+
+function incrementToTarget(start = 4, endValue, trigger, callback) {
+ if (!trigger) return;
+ const duration = 500;
+ const steps = endValue - start;
+ if (steps <= 0) return;
+ const stepTime = duration / steps;
+ let current = start;
+ const interval = setInterval(() => {
+  current++;
+  callback(current);
+  if (current >= endValue) clearInterval(interval);
+ }, stepTime);
 }
--- src/components/HomepageFeatures/category/SubCategory.jsx
+++ src/components/HomepageFeatures/category/SubCategory.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-import "./SubCategory.css";
-import denem from "@site/static/img/web3ogren-banner.jpg";
-import DenemSvg from "@site/static/img/kilt.svg";
+import "./css/SubCategory.css";
 
-export default function SubCategory() {
+export default function SubCategory(SubCategoryData) {
+ let Data = SubCategoryData.SubCategoryData;
  return (
-  <div className="sub-category-main-container">
-   {/* <img className="sub-category-image" src={denem} /> */}
-   <DenemSvg className="sub-category-image" />
-   <h1 className="sub-category-h1">Lorem ipsum dolor sit amet.</h1>
-   <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum magnam
-    officiis explicabo blanditiis ullam. Nihil animi cupiditate, facere aperiam
-    inventore repellat laudantium illum. Maxime, pariatur.
-   </p>
-  </div>
+  <a href={Data.href}>
+   <div className="sub-category-main-container">
+    {Data?.Svg ? (
+     <Data.Svg className="sub-category-image" />
+    ) : (
+     Data.imgUrl && (
+      <img
+       src={"http://localhost:3000" + Data.imgUrl}
+       alt={Data.title}
+       className="sub-category-image"
+      />
+     )
+    )}
+    <h1 className="sub-category-h1">{Data?.title}</h1>
+    <p>{Data?.description}</p>
+   </div>
+  </a>
  );
 }
